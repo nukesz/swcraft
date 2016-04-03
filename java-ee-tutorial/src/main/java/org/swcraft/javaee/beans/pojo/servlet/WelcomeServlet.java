9e40a7bf584d0d2b25a2c50644f759d0c657aa11
Norbert Benczur <norbert.benczur@gmail.com>
2016-04-03 18:47:09 +0200

    Renamed Applications to servlets

---
+++ java-ee-tutorial/src/main/java/org/swcraft/javaee/beans/pojo/servlet/WelcomeServlet.java
@@ -0,0 +1,25 @@
+package org.swcraft.javaee.beans.pojo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.swcraft.javaee.beans.pojo.services.HelloService;
+
+@WebServlet("/hello")
+public class WelcomeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	private final HelloService service = new HelloService();
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.getWriter().println(service.sayHello());
+    }
+	
+}
