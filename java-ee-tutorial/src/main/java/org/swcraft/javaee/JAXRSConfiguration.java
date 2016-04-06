dbdd5d7af2135290ded57f5400c8818105c2ce71
Norbert Benczur <norbert.benczur@ericsson.com>
2016-04-06 13:35:41 +0200

    Added class to show how to use EJBs in client

---
+++ java-ee-tutorial/src/main/java/org/swcraft/javaee/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package org.swcraft.javaee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+
+}
---
+++ java-ee-tutorial/src/main/java/org/swcraft/javaee/beans/ejb/usage/ServerManagedBeanClient.java
@@ -0,0 +1,49 @@
+package org.swcraft.javaee.beans.ejb.usage;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.swcraft.javaee.beans.ejb.services.HelloServiceBean;
+
+
+@Stateless
+@Path("client")
+public class ServerManagedBeanClient {
+
+	@EJB
+	HelloServiceBean serviceWithEJB;
+	
+	@Inject
+	HelloServiceBean serviceWithInject;
+	
+	@GET
+	@Path("/ejb")
+	public String ejbClient() {
+		return "EJB: " + serviceWithEJB.sayHello();
+	}
+	
+	@GET
+	@Path("/inject")
+	public String injectClient() {
+		return "Inject: " + serviceWithInject.sayHello();
+	}
+	
+	@GET
+	@Path("/jndi")
+	public String jdniClient() {
+		try {
+			InitialContext ic = new InitialContext();
+			HelloServiceBean service = (HelloServiceBean) ic.lookup("java:module/HelloServiceBean");
+			return "Jndi: " + service.sayHello();
+		} catch (NamingException e) {
+			e.printStackTrace();
+			return "error?";
+		}
+
+	}
+}
