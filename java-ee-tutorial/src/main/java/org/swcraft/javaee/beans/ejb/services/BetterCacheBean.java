4410bad7875e43d778f97ee28b420846e4ab19e7
Norbert Benczur <norbert.benczur@ericsson.com>
2016-04-07 13:45:50 +0200

    Added SingletonBean with ConcurrencyManagement

---
+++ java-ee-tutorial/src/main/java/org/swcraft/javaee/beans/ejb/services/BetterCacheBean.java
@@ -0,0 +1,27 @@
+package org.swcraft.javaee.beans.ejb.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+
+@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER) // Default
+@Singleton
+public class BetterCacheBean {
+	
+	private List<String> data = new ArrayList<>();
+	
+	@Lock(LockType.WRITE)
+	public void add(String value) {
+		data.add(value);
+	}
+	
+	@Lock(LockType.READ)
+	public List<String> getData() {
+		return data;
+	}
+}
