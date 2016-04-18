a90bf20e4bc22580050441f047be34ce0c435300
Norbert Benczur <norbert.benczur@gmail.com>
2016-04-18 19:06:33 +0200

    Added examples for filtering on streams

---
+++ java-8-tutorial/src/main/java/org/swcraft/java8/data/SampleData.java
@@ -0,0 +1,8 @@
+package org.swcraft.java8.data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SampleData {
+    public static final List<Integer> numbers = Arrays.asList(1, 3, 2, -6, 7, -8, 4);
+}
---
+++ java-8-tutorial/src/main/java/org/swcraft/java8/streams/LambdaPositiveNumberCounter.java
@@ -0,0 +1,18 @@
+package org.swcraft.java8.streams;
+
+import java.util.List;
+
+import org.swcraft.java8.data.SampleData;
+
+public class LambdaPositiveNumberCounter {
+
+    private static long getCountOfPositiveNumbers(List<Integer> list) {
+        return list.stream()
+                   .filter(number -> number > 0)
+                   .count();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCountOfPositiveNumbers(SampleData.numbers));
+    }
+}
